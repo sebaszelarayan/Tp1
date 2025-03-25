7ac8828049c1da30f0bd5db225e2626cc4b6e313
Your Nam <you@example.com>
2025-03-25 16:18:46 -0300

    Punteros y Varibales

--- tp1_1.c
+++ tp1_1.c
@@ -2,6 +2,21 @@
 
 int main()
 {
-    printf("Hola Mundo");
+    int *punteroNum;
+    int num=8;
+
+    punteroNum = &num;
+    printf("\nHola Mundo\n")
+    ;
+    printf("Contenido del Puntero: %d\n",*punteroNum);
+
+    printf("Direccion de Memoria Almacenada por el puntero: %p\n",punteroNum);
+
+    printf("Direccion Variable: %p\n",&num);
+
+    printf("Direccion Puntero: %p\n",&punteroNum);
+
+    printf("Tmaño utilizado por la varibale: %d\n",sizeof(num));
+    
     return 0;
 }
